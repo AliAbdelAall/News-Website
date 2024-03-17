41f432db8325464e90771c891659ebe7568917f7
AliAbdelAall <3laych@gmail.com>
2024-03-17 17:09:34 +0200

    updated loadNews function to jQuery

--- frontend/scripts/news.js
+++ frontend/scripts/news.js
@@ -4,22 +4,39 @@ const content_input = $("#content-input")
 const news_container = $("#news-container")
 
 const loadNews = () => {
-  fetch("http://127.0.0.1/News-Website/backend/get-news.php", {
-    method: 'GET'
-  })
-    .then((response) => {
-      return response.json()
-
-    }).then((data) => {
+  $.ajax({
+    url: "http://127.0.0.1/News-Website/backend/get-news.php",
+    method: "GET",
+    dataType: "json",
+    success: (data) => {
+      console.log(data)
       displayNews(data)
-
-    }).catch((error) => {
+    },
+    error: (error) => {
       console.error(error)
-    })
-}
+    }
+  })
+  // fetch("http://127.0.0.1/News-Website/backend/get-news.php", {
+  //   method: 'GET'
+  // })
+  //   .then((response) => {
+  //     return response.json()
 
+  //   }).then((data) => {
+  //     console.log(data)
+  //     // displayNews(data)
+
+  //   }).catch((error) => {
+  //     console.error(error)
+  //   })
+}
+// loadNews()
 // const displayNews = (data) => {
 //   news_container.empty()
+//   $.each(data.news, (i, news) => {
+//     const news_div = $("<div>").addClass("flex column gap-10 news-wrapper")
 
+//     const
 
+//   })
 // }
